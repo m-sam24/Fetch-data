8f41c2982c778ab3d623bf5d1b83c75da31363a2
m-sam24 <samyakmehta23@gmail.com>
2025-06-11 13:00:25 +0530

    Add files via upload
    The first cut for my project

---
+++ script.js
@@ -0,0 +1,33 @@
+let users = [];
+
+async function fetchUsers() {
+    const response = await fetch('https://684905e245f4c0f5ee6fb988.mockapi.io/api/V1/UserData');
+    const data = await response.json();
+    users = data;
+    console.log(users.map(u => u.Name));
+    const container = document.querySelector('.items-container');
+    container.innerHTML = ''; // Clear previous content
+    users.forEach(user => {
+        const markup = `
+            <div class="item-container">
+                <div class="img-wrapper">
+                    <img src="${user.Image}" alt="Avatar">
+                </div>
+                <h3>${user.Name}</h3>
+                <p>${user.Email}</p>
+                <p>${user.Message}</p>
+                <button class="delete-btn">X</button>
+            </div>
+        `;
+        container.insertAdjacentHTML('beforeend', markup);
+    });
+
+    container.addEventListener('click', function(e) {
+        if (e.target.classList.contains('delete-btn')) {
+            const card = e.target.closest('.item-container');
+            card.remove();
+        }
+    });
+}
+
+fetchUsers();
